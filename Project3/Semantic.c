3932c8c5102ce61d7165538fe31ba2954a623815
Unkrible <715534986@qq.com>
2017-12-24 16:22:07 +0800

    Modify Something

--- Project3/Semantic.c
+++ Project3/Semantic.c
@@ -722,14 +722,12 @@ Type Exp(Node *n, Operand place){
 	}
 	else if(strcmp(child->identifier,"MINUS")==0){
 
-		//--------------------------translate----------------
-			Operand rightOp = malloc(sizeof(Operand_));
-			memset(rightOp, 0, sizeof(Operand_));
-			rightOp->kind = TEMPVAR;
-			rightOp->u.var_no = temVarNo;
-			int rightOpNo = rightOp->u.var_no;
-			temVarNo++;
-		//--------------------------translate-----------------
+		Operand rightOp = malloc(sizeof(Operand_));
+		memset(rightOp, 0, sizeof(Operand_));
+		rightOp->kind = TEMPVAR;
+		rightOp->u.var_no = temVarNo;
+		int rightOpNo = rightOp->u.var_no;
+		temVarNo++;
 
 		//Exp->MINUS Exp
 		child = child->sibling;
@@ -742,22 +740,19 @@ Type Exp(Node *n, Operand place){
 			return NULL;
 		}
 
-		//--------------------------translate-----------------
-			Operand zeroOp =  malloc(sizeof(Operand_));
-			memset(zeroOp, 0, sizeof(Operand_));
-			zeroOp->kind = CONSTANT;
-			zeroOp->u.value = zeroStr;
-			// place := #0 - Exp
-			if(place!=NULL){
-				InterCode minusCode = malloc(sizeof(InterCode_));
-				memset(minusCode, 0, sizeof(InterCode_));
-				minusCode->kind = SUB_N;
-				minusCode->u.binop.result = place;
-				minusCode->u.binop.op1 = zeroOp;
-				minusCode->u.binop.op2 = rightOp;
-				insertCode(minusCode);
-			}
-		//--------------------------translate-----------------
+		Operand zeroOp =  malloc(sizeof(Operand_));
+		memset(zeroOp, 0, sizeof(Operand_));
+		zeroOp->kind = CONSTANT;
+		zeroOp->u.value = zeroStr;
+		if(place!=NULL){
+			InterCode minusCode = malloc(sizeof(InterCode_));
+			memset(minusCode, 0, sizeof(InterCode_));
+			minusCode->kind = SUB_N;
+			minusCode->u.binop.result = place;
+			minusCode->u.binop.op1 = zeroOp;
+			minusCode->u.binop.op2 = rightOp;
+			insertCode(minusCode);
+		}
 
 		type->assign = RIGHT;
 		return type;
@@ -780,13 +775,13 @@ Type Exp(Node *n, Operand place){
 		c0->u.value= zeroStr;
 		code0->u.assign.right=c0;
 		if(place!=NULL)
-			insertCode(code0);	//code0
-		Type t=Exp_Cond(n,lb1,lb2);	//code1
+			insertCode(code0);
+		Type t=Exp_Cond(n,lb1,lb2);
 
 		InterCode lb1code=malloc(sizeof(InterCode_));
 		lb1code->kind=LABEL_N;
 		lb1code->u.sinop.op=lb1;
-		insertCode(lb1code);	//label 1
+		insertCode(lb1code);
 
 		Operand c1=malloc(sizeof(Operand_));
 		c1->kind=CONSTANT;
@@ -796,7 +791,7 @@ Type Exp(Node *n, Operand place){
 		code2->u.assign.left=place;
 		code2->u.assign.right=c1;
 		if(place!=NULL)
-			insertCode(code2);		//code2
+			insertCode(code2);
 		InterCode lb2code=malloc(sizeof(InterCode_));
 		lb2code->kind=LABEL_N;
 		lb2code->u.sinop.op=lb2;
@@ -813,12 +808,11 @@ Type Exp(Node *n, Operand place){
 				return NULL;
 			}
 
-			//--------------------------translate------------
 			if(place!=NULL){
 				place->kind = VARIABLE;
 				place->u.value = child->value;
 			}
-			//--------------------------translate------------
+
 			value->assign = BOTH;
 			return value;
 		}
@@ -845,7 +839,7 @@ Type Exp(Node *n, Operand place){
 					return NULL;
 				}
 				else{
-					//--------------------------translate----
+
 					if(strcmp(func->u.function->name,"read")==0){
 						if(place!=NULL){
 							InterCode funcCode = malloc(sizeof(InterCode_));
@@ -883,22 +877,18 @@ Type Exp(Node *n, Operand place){
 							insertCode(funcCode);
 						}
 					}
-					//--------------------------translate----
 				}
 			}
 			else{
 
-				//--------------------------translate--------
 				Operand argsListHead=malloc(sizeof(Operand_));
 				argsListHead->next=NULL;
-				//--------------------------translate--------
 
 				if(Args(child,param,argsListHead)!=0){  //比较两个类型是否匹配
 					printf("Error type 9 at Line %d: Function \"%s\" is not applicable for arguments.\n", child->line, func->u.function->name);
 					return NULL;
 				}
 				else{
-					//--------------------------translate----
 					if(strcmp(func->u.function->name,"write")==0){
 						InterCode funcCode = malloc(sizeof(InterCode_));
 						memset(funcCode, 0, sizeof(InterCode_));
@@ -909,8 +899,6 @@ Type Exp(Node *n, Operand place){
 					else{
 						Operand argsP = argsListHead;
 						while(argsP!=NULL){
-							// PARAM argsP
-							//直接按照队列方向即从右向左入栈
 							InterCode argCode = malloc(sizeof(InterCode_));
 							memset(argCode, 0, sizeof(InterCode_));
 							argCode->kind = ARG_N;
@@ -945,7 +933,6 @@ Type Exp(Node *n, Operand place){
 							insertCode(funcCode);
 						}
 					}
-					//--------------------------translate----
 				}
 			}
 			Type rtn = (Type)malloc(sizeof(struct Type_));
@@ -961,12 +948,10 @@ Type Exp(Node *n, Operand place){
 		type->u.basic = TYPE_INT;
 		type->assign = RIGHT;
 
-		//--------------------------translate-----------------
-			if(place!=NULL){
-				place->kind = CONSTANT;
-				place->u.value = child->value;
-			}
-		//--------------------------translate-----------------
+		if(place!=NULL){
+			place->kind = CONSTANT;
+			place->u.value = child->value;
+		}
 
 		return type;
 	}
@@ -977,12 +962,10 @@ Type Exp(Node *n, Operand place){
 		type->u.basic = TYPE_FLOAT;
 		type->assign = RIGHT;
 
-		//--------------------------translate-----------------
-			if(place!=NULL){
-				place->kind = CONSTANT;
-				place->u.value = child->value;
-			}
-		//--------------------------translate-----------------
+		if(place!=NULL){
+			place->kind = CONSTANT;
+			place->u.value = child->value;
+		}
 
 		return type;
 	}
@@ -991,20 +974,18 @@ Type Exp(Node *n, Operand place){
 		if(strcmp(child->sibling->identifier,"ASSIGNOP")==0 ){
 			//Exp->Exp ASSIGNOP Exp
 
-			//--------------------------translate-------------
-				Operand leftOp = malloc(sizeof(Operand_));
-				memset(leftOp, 0, sizeof(Operand_));
-				leftOp->kind = TEMPVAR;
-				leftOp->u.var_no = temVarNo;
-				temVarNo++;
+			Operand leftOp = malloc(sizeof(Operand_));
+			memset(leftOp, 0, sizeof(Operand_));
+			leftOp->kind = TEMPVAR;
+			leftOp->u.var_no = temVarNo;
+			temVarNo++;
 
-				Operand rightOp = malloc(sizeof(Operand_));
-				memset(rightOp, 0, sizeof(Operand_));
-				rightOp->kind = TEMPVAR;
-				rightOp->u.var_no = temVarNo;
-				int rightOpNo = rightOp->u.var_no;
-				temVarNo++;
-			//--------------------------translate-------------
+			Operand rightOp = malloc(sizeof(Operand_));
+			memset(rightOp, 0, sizeof(Operand_));
+			rightOp->kind = TEMPVAR;
+			rightOp->u.var_no = temVarNo;
+			int rightOpNo = rightOp->u.var_no;
+			temVarNo++;
 
 			//判断表达式左边是否为左值
 			Type lhs = Exp(child,leftOp);
@@ -1020,32 +1001,25 @@ Type Exp(Node *n, Operand place){
 
 			//判断赋值号两边表达式是否类型匹配
 			if(typeEqual(lhs, rhs)==0){
-
-				//--------------------------translate---------
-					if( !(rightOp->kind==TEMPVAR && rightOp->u.var_no==rightOpNo && (leftOp->kind==TEMPVAR || leftOp->kind==VARIABLE)) ){
-						//rightOp被改 说明需要生成赋值语句
-						// leftOp := rightOp
-						InterCode assignCode1 = malloc(sizeof(InterCode_));
-						memset(assignCode1, 0, sizeof(InterCode_));
-						assignCode1->kind = ASSIGN_N;
-						assignCode1->u.assign.left = leftOp;
-						assignCode1->u.assign.right = rightOp;
-						insertCode(assignCode1);
-					}
-					else{
-						//否则将rightOp替换为leftOp即可
-						memcpy(rightOp, leftOp, sizeof(Operand_));
-					}
-					if(place!=NULL){
-						//place := rightOp
-						InterCode assignCode2 = malloc(sizeof(InterCode_));
-						memset(assignCode2, 0, sizeof(InterCode_));
-						assignCode2->kind = ASSIGN_N;
-						assignCode2->u.assign.left = place;
-						assignCode2->u.assign.right = rightOp;
-						insertCode(assignCode2);
-					}
-				//--------------------------translate---------
+				if( !(rightOp->kind==TEMPVAR && rightOp->u.var_no==rightOpNo && (leftOp->kind==TEMPVAR || leftOp->kind==VARIABLE)) ){
+					InterCode assignCode1 = malloc(sizeof(InterCode_));
+					memset(assignCode1, 0, sizeof(InterCode_));
+					assignCode1->kind = ASSIGN_N;
+					assignCode1->u.assign.left = leftOp;
+					assignCode1->u.assign.right = rightOp;
+					insertCode(assignCode1);
+				}
+				else{
+					memcpy(rightOp, leftOp, sizeof(Operand_));
+				}
+				if(place!=NULL){
+					InterCode assignCode2 = malloc(sizeof(InterCode_));
+					memset(assignCode2, 0, sizeof(InterCode_));
+					assignCode2->kind = ASSIGN_N;
+					assignCode2->u.assign.left = place;
+					assignCode2->u.assign.right = rightOp;
+					insertCode(assignCode2);
+				}
 
 				return lhs;
 			}
@@ -1063,18 +1037,16 @@ Type Exp(Node *n, Operand place){
 						strcmp(child->sibling->identifier,"DIV")==0){
 			//Exp->Exp AND|OR|RELOP|PLUS|MINUS|STAR|DIV Exp
 
-			//--------------------------translate-------------
-				Operand leftOp = malloc(sizeof(Operand_));
-				memset(leftOp, 0, sizeof(Operand_));
-				leftOp->kind = TEMPVAR;
-				leftOp->u.var_no = temVarNo;
-				temVarNo++;
-				Operand rightOp = malloc(sizeof(Operand_));
-				memset(rightOp, 0, sizeof(Operand_));
-				rightOp->kind = TEMPVAR;
-				rightOp->u.var_no = temVarNo;
-				temVarNo++;
-			//--------------------------translate-------------
+			Operand leftOp = malloc(sizeof(Operand_));
+			memset(leftOp, 0, sizeof(Operand_));
+			leftOp->kind = TEMPVAR;
+			leftOp->u.var_no = temVarNo;
+			temVarNo++;
+			Operand rightOp = malloc(sizeof(Operand_));
+			memset(rightOp, 0, sizeof(Operand_));
+			rightOp->kind = TEMPVAR;
+			rightOp->u.var_no = temVarNo;
+			temVarNo++;
 
 			Type lhs = Exp(child,leftOp);
 			Type rhs = Exp(child->sibling->sibling,rightOp);
@@ -1086,7 +1058,6 @@ Type Exp(Node *n, Operand place){
 			if(lhs->kind==BASIC && rhs->kind==BASIC &&
 							lhs->u.basic==rhs->u.basic){
 
-				//--------------------------translate---------
 				if(place!=NULL){
 					InterCode calcCode = malloc(sizeof(InterCode_));
 					memset(calcCode, 0, sizeof(InterCode_));
@@ -1105,7 +1076,6 @@ Type Exp(Node *n, Operand place){
 					calcCode->u.binop.op2 = rightOp;
 					insertCode(calcCode);
 				}
-				//--------------------------translate---------
 
 				lhs->assign = RIGHT;
 				return lhs;
@@ -1116,15 +1086,11 @@ Type Exp(Node *n, Operand place){
 			}
 		}
 		else if(strcmp(child->sibling->identifier,"LB")==0){
-
-			//--------------------------translate-------------
-				//用于存储基地址
 				Operand baseOp = malloc(sizeof(Operand_));
 				memset(baseOp, 0, sizeof(Operand_));
 				baseOp->kind = TEMPVAR;
 				baseOp->u.var_no = temVarNo;
 				temVarNo++;
-			//--------------------------translate-------------
 
 			//Exp->Exp LB Exp RB
 			Type array=Exp(child,baseOp);
@@ -1136,20 +1102,17 @@ Type Exp(Node *n, Operand place){
 				return NULL;
 			}
 
-			//--------------------------translate-------------
-				int subscipt = 1;
-				if(strcmp(child->sibling->sibling->child->identifier, "INT")==0)
-					subscipt = atoi(child->sibling->sibling->child->value);
-				Operand subscriptOp=NULL;
-				if(subscipt!=0){
-					//用于存储数组下标
-					subscriptOp = malloc(sizeof(Operand_));
-					memset(subscriptOp, 0, sizeof(Operand_));
-					subscriptOp->kind = TEMPVAR;
-					subscriptOp->u.var_no = temVarNo;
-					temVarNo++;
-				}
-			//--------------------------translate-------------
+			int subscipt = 1;
+			if(strcmp(child->sibling->sibling->child->identifier, "INT")==0)
+				subscipt = atoi(child->sibling->sibling->child->value);
+			Operand subscriptOp=NULL;
+			if(subscipt!=0){
+				subscriptOp = malloc(sizeof(Operand_));
+				memset(subscriptOp, 0, sizeof(Operand_));
+				subscriptOp->kind = TEMPVAR;
+				subscriptOp->u.var_no = temVarNo;
+				temVarNo++;
+			}
 
 			child = child->sibling->sibling;
 			Type arrayNumber = Exp(child,subscriptOp);
@@ -1160,81 +1123,70 @@ Type Exp(Node *n, Operand place){
 				return NULL;
 			}
 
-			//--------------------------translate-------------
-				//用于存储计算出的偏移量
-				Operand offsetOp = malloc(sizeof(Operand_));
-				memset(offsetOp, 0, sizeof(Operand_));
-				offsetOp->kind = TEMPVAR;
-				offsetOp->u.var_no = temVarNo;
-				temVarNo++;
-				if(subscipt!=0){
-				//用于存储宽度
-					Operand wideOp = malloc(sizeof(Operand_));
-					memset(wideOp, 0, sizeof(Operand_));
-					wideOp->kind = CONSTANT;
-					char* wideStr = malloc(16*sizeof(char));
-					int wide = array->u.array.size;
-					sprintf(wideStr, "%d", wide);
-					wideOp->u.value = wideStr;
+			Operand offsetOp = malloc(sizeof(Operand_));
+			memset(offsetOp, 0, sizeof(Operand_));
+			offsetOp->kind = TEMPVAR;
+			offsetOp->u.var_no = temVarNo;
+			temVarNo++;
+			if(subscipt!=0){
+				Operand wideOp = malloc(sizeof(Operand_));
+				memset(wideOp, 0, sizeof(Operand_));
+				wideOp->kind = CONSTANT;
+				char* wideStr = malloc(16*sizeof(char));
+				int wide = array->u.array.size;
+				sprintf(wideStr, "%d", wide);
+				wideOp->u.value = wideStr;
 
-					//offsetOp := subscriptOp MUL wideOp
-					InterCode offsetCode = malloc(sizeof(InterCode_));
-					memset(offsetCode, 0, sizeof(InterCode_));
-					offsetCode->kind = MUL_N;
-					offsetCode->u.binop.result = offsetOp;
-					offsetCode->u.binop.op1 = subscriptOp;
-					offsetCode->u.binop.op2 = wideOp;
-					insertCode(offsetCode);
+				InterCode offsetCode = malloc(sizeof(InterCode_));
+				memset(offsetCode, 0, sizeof(InterCode_));
+				offsetCode->kind = MUL_N;
+				offsetCode->u.binop.result = offsetOp;
+				offsetCode->u.binop.op1 = subscriptOp;
+				offsetCode->u.binop.op2 = wideOp;
+				insertCode(offsetCode);
 
-					// 地址 := baseOp ADD offsetOp
-					InterCode addrCode = malloc(sizeof(InterCode_));
-					memset(addrCode, 0, sizeof(InterCode_));
-					addrCode->kind = ADD_N;
-					addrCode->u.binop.op1 = baseOp;
-					addrCode->u.binop.op2 = offsetOp;
-					if(array->u.array.elem->kind==BASIC){
-						//如果下一层是BASIC 则place是地址指向的位置
-						Operand temAddrOp = malloc(sizeof(Operand_));
-						memset(temAddrOp, 0, sizeof(Operand_));
-						temAddrOp->kind = TEMPVAR;
-						temAddrOp->u.var_no = temVarNo;
-						temVarNo++;
+				InterCode addrCode = malloc(sizeof(InterCode_));
+				memset(addrCode, 0, sizeof(InterCode_));
+				addrCode->kind = ADD_N;
+				addrCode->u.binop.op1 = baseOp;
+				addrCode->u.binop.op2 = offsetOp;
+				if(array->u.array.elem->kind==BASIC){
+					Operand temAddrOp = malloc(sizeof(Operand_));
+					memset(temAddrOp, 0, sizeof(Operand_));
+					temAddrOp->kind = TEMPVAR;
+					temAddrOp->u.var_no = temVarNo;
+					temVarNo++;
 
-						addrCode->u.binop.result = temAddrOp;
-						place->kind = TADDRESS;
-						place->u.addr = temAddrOp;
-					}
-					else{
-						//如果下一层仍然是数组 则place是地址
-						addrCode->u.binop.result = place;
-					}
-					insertCode(addrCode);
+					addrCode->u.binop.result = temAddrOp;
+					place->kind = TADDRESS;
+					place->u.addr = temAddrOp;
 				}
 				else{
-					// 地址 := baseOp
-					InterCode addrCode = malloc(sizeof(InterCode_));
-					memset(addrCode, 0, sizeof(InterCode_));
-					addrCode->kind = ASSIGN_N;
-					addrCode->u.assign.right = baseOp;
-					if(array->u.array.elem->kind==BASIC){
-						//如果下一层是BASIC 则place是地址指向的位置
-						Operand temAddrOp = malloc(sizeof(Operand_));
-						memset(temAddrOp, 0, sizeof(Operand_));
-						temAddrOp->kind = TEMPVAR;
-						temAddrOp->u.var_no = temVarNo;
-						temVarNo++;
-
-						addrCode->u.assign.left = temAddrOp;
-						place->kind = TADDRESS;
-						place->u.addr = temAddrOp;
-					}
-					else{
-						//如果下一层仍然是数组 则place是地址
-						addrCode->u.assign.left = place;
-					}
-					insertCode(addrCode);
+					addrCode->u.binop.result = place;
 				}
-			//--------------------------translate-------------
+				insertCode(addrCode);
+			}
+			else{
+				InterCode addrCode = malloc(sizeof(InterCode_));
+				memset(addrCode, 0, sizeof(InterCode_));
+				addrCode->kind = ASSIGN_N;
+				addrCode->u.assign.right = baseOp;
+				if(array->u.array.elem->kind==BASIC){
+					Operand temAddrOp = malloc(sizeof(Operand_));
+					memset(temAddrOp, 0, sizeof(Operand_));
+					temAddrOp->kind = TEMPVAR;
+					temAddrOp->u.var_no = temVarNo;
+					temVarNo++;
+
+					addrCode->u.assign.left = temAddrOp;
+					place->kind = TADDRESS;
+					place->u.addr = temAddrOp;
+				}
+				else{
+					addrCode->u.assign.left = place;
+				}
+				insertCode(addrCode);
+			}
 
 			Type rtn = (Type)malloc(sizeof(struct Type_));
 			memcpy(rtn, array->u.array.elem, sizeof(struct Type_));
@@ -1243,13 +1195,11 @@ Type Exp(Node *n, Operand place){
 		}
 		else if(strcmp(child->sibling->identifier,"DOT")==0){
 
-			//--------------------------translate-------------
-				Operand strucVarOp = malloc(sizeof(Operand_));
-				memset(strucVarOp, 0, sizeof(Operand_));
-				strucVarOp->kind = TEMPVAR;
-				strucVarOp->u.var_no = temVarNo;
-				temVarNo++;
-			//--------------------------translate-------------
+			Operand strucVarOp = malloc(sizeof(Operand_));
+			memset(strucVarOp, 0, sizeof(Operand_));
+			strucVarOp->kind = TEMPVAR;
+			strucVarOp->u.var_no = temVarNo;
+			temVarNo++;
 
 			//Exp->Exp DOT ID
 			Type structure = Exp(child,strucVarOp);
@@ -1267,7 +1217,6 @@ Type Exp(Node *n, Operand place){
 			while(structDomain!=NULL){
 				if(strcmp(structDomain->name, child->value) ==0){
 
-					//--------------------------translate-----
 					if(offset==0){
 						if(place!=NULL){
 							if(structDomain->type->kind==BASIC){
@@ -1280,21 +1229,18 @@ Type Exp(Node *n, Operand place){
 						}
 					}
 					else{
-						//用于存储偏移量
 						Operand offsetOp = malloc(sizeof(Operand_));
 						memset(offsetOp, 0, sizeof(Operand_));
 						offsetOp->kind = CONSTANT;
 						char* offsetStr = malloc(16*sizeof(char));
 						sprintf(offsetStr, "%d", offset);
 						offsetOp->u.value = offsetStr;
-						// 地址 := strucVarOp ADD offsetOp
 						InterCode addrCode = malloc(sizeof(InterCode_));
 						memset(addrCode, 0, sizeof(InterCode_));
 						addrCode->kind = ADD_N;
 						addrCode->u.binop.op1 = strucVarOp;
 						addrCode->u.binop.op2 = offsetOp;
 						if(structure->kind==BASIC){
-							//如果下一层是BASIC 则place是地址指向的位置
 							Operand temAddrOp = malloc(sizeof(Operand_));
 							memset(temAddrOp, 0, sizeof(Operand_));
 							temAddrOp->kind = TEMPVAR;
@@ -1306,12 +1252,10 @@ Type Exp(Node *n, Operand place){
 							place->u.addr = temAddrOp;
 						}
 						else{
-							//如果下一层仍然是struct 则place是地址
 							addrCode->u.binop.result = place;
 						}
 						insertCode(addrCode);
 					}
-					//--------------------------translate-----
 
 					Type rtn = (Type)malloc(sizeof(struct Type_));
 					memcpy(rtn, structDomain->type, sizeof(struct Type_));
